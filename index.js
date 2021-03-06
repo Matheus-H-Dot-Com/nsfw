2b47cc21f3659aeee981967ea177aef81c0283bb
Matheus-H-Dot-Com <79377901+Matheus-H-Dot-Com@users.noreply.github.com>
2021-03-06 20:45:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -603,7 +603,8 @@ case 'timer':
         }).catch(err => {
             console.error(err);
             return ReE(res, err.message, 500);
-        }
+        })
+			        break
 				case 'pesquisaen':
 					if (args.length < 1) return reply('digite palavras-chave')
 					tels = body.slice(8)		
