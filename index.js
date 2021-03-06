d5f5d6ff47f58a4d336ac9a4f9ac07617e3fc854
Matheus-H-Dot-Com <79377901+Matheus-H-Dot-Com@users.noreply.github.com>
2021-03-06 19:08:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -594,7 +594,7 @@ case 'timer':
 					tels = body.slice(6)	
                                         if (!isUser) return reply(mess.only.daftarB)				
 					anu = await fetchJson(`https://pt.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=${tels}`, {method: 'get'})
-					reply(anu.*)
+					reply(anu.text/x-wiki)
 					break	
 				case 'pesquisaen':
 					if (args.length < 1) return reply('digite palavras-chave')
