c20d085954fb42cbe7f2fba1ab52dfaf92d826fd
Matheus-H-Dot-Com <79377901+Matheus-H-Dot-Com@users.noreply.github.com>
2021-03-07 02:14:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -596,7 +596,6 @@ case 'timer':
 					tels = body.slice(6)	
                                         if (!isUser) return reply(mess.only.daftarB)				
 					wiki = await fetchJson(`https://api.duckduckgo.com/?q=${tels}&format=json&pretty=1&no_html=1&skip_disambig=1`, {method: 'get'})
-					const json ='{"Result": "Text", "Abstract"}';
 					const obj = JSON.parse(wiki);
 					reply(wiki.Result)
 					break	
