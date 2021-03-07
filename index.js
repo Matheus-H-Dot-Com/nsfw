4c0e327a7b748104d8b84491cc7c4a59ec58ac19
Matheus-H-Dot-Com <79377901+Matheus-H-Dot-Com@users.noreply.github.com>
2021-03-06 22:10:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -596,7 +596,7 @@ case 'timer':
 					tels = body.slice(6)	
                                         if (!isUser) return reply(mess.only.daftarB)				
 					anu = await fetchJson(`https://api.duckduckgo.com/?skip_disambig=1&format=json&pretty=5&q=${tels}`, {method: 'getJSON'})
-					reply(anu.extract)
+					reply(anu.Text)
 				        then(res => console.log(res))
 				        then(json => {
                                         console.log('res', json);
