3602340db197185e3e7b7df05a5bed1fbb54fd18
Matheus-H-Dot-Com <79377901+Matheus-H-Dot-Com@users.noreply.github.com>
2021-03-07 00:56:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -595,7 +595,7 @@ case 'timer':
                                         if (args.length < 1) return reply('digite palavras-chave')
 					tels = body.slice(6)	
                                         if (!isUser) return reply(mess.only.daftarB)				
-					anu = await fetchJson(`https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exlimit=1&explaintext&titles=pudding`, {method: 'getJSON'})
+					anu = await fetchJson(`https://pt.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exlimit=1&explaintext&titles=${tels}`, {method: 'getJSON'})
 					reply(anu.extract)
 					break	
 			       case 'pesquisaen':
