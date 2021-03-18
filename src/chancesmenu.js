1696da80bfed6f2692a3ec8008112a0e14a601b4
Matheus-H-Dot-Com <79377901+Matheus-H-Dot-Com@users.noreply.github.com>
2021-03-18 12:17:51 -0300

    Update chancesmenu.js

--- src/chancesmenu.js
+++ src/chancesmenu.js
@@ -1,12 +1,12 @@
 const chancesmenu = (prefix, pushname) => {
-    return `Opa, esse aqui é o menu de chances, não se esqueça de marcar a pessoa!'
+    return `Opa, esse aqui é o menu de chances, não se esqueça de marcar a pessoa!
     ◪ *CHANCES E ZOEIRAS*
   │
   └─ ❏ ${prefix}peidou
   └─ ❏ ${prefix}tempocama
   └─ ❏ ${prefix}%sapatao
   └─ ❏ ${prefix}%gay
-  └─ ❏ ${prefix}%namoraesseano
+  └─ ❏ ${prefix}%namoraesseano'
 
 
 }
