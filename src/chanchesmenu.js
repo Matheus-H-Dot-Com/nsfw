e89bb2c55b805dfca2a9f3115151a945c263cddd
Matheus-H-Dot-Com <79377901+Matheus-H-Dot-Com@users.noreply.github.com>
2021-03-18 12:02:54 -0300

    Create chanchesmenu.js

---
+++ src/chanchesmenu.js
@@ -0,0 +1,14 @@
+const chancesmenu = (prefix, pushname) => {
+    return `Opa, esse aqui é o menu de chances, não se esqueça de marcar a pessoa!'
+    ◪ *CHANCES E ZOEIRAS*
+  │
+  └─ ❏ ${prefix}peidou
+  └─ ❏ ${prefix}tempocama
+  └─ ❏ ${prefix}%sapatao
+  └─ ❏ ${prefix}%gay
+  └─ ❏ ${prefix}%namoraesseano
+
+
+}
+
+exports.chancesmenu = chanchesmenu
